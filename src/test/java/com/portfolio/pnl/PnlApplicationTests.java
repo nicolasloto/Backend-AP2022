d5579021e2ae1d993413c26bce3c0298459cd666
Nicolás Loto <busslxtx@gmail.com>
2022-10-04 17:00:51 -0300

    Initial commit

---
+++ src/test/java/com/portfolio/pnl/PnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.pnl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
