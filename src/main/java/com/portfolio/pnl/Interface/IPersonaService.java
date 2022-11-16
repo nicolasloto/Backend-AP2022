b59edb8523b6d87cd1ed5985ad5d73980b29d430
Nicolás Loto <busslxtx@gmail.com>
2022-11-16 16:46:51 -0300

    creación de estructura + login jwt (wip)

---
+++ src/main/java/com/portfolio/pnl/Interface/IPersonaService.java
@@ -0,0 +1,11 @@
+package com.portfolio.pnl.Interface;
+
+import com.portfolio.pnl.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    public void savePersona(Persona persona);
+    public void deletePersona(Long id);
+    public Persona findPersona(Long id);
+}
---
+++ src/main/java/com/portfolio/pnl/Security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.pnl.Security.Repository;
+
+import com.portfolio.pnl.Security.Entity.Rol;
+import com.portfolio.pnl.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
