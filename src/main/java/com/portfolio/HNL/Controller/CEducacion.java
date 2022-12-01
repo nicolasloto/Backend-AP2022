92fd210ba7441f86f95055aa62b24283d7141639
Nicolás Loto <busslxtx@gmail.com>
2022-12-01 20:25:46 -0300

    CORS WebConfig

--- src/main/java/com/portfolio/HNL/Controller/CEducacion.java
+++ src/main/java/com/portfolio/HNL/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frontend-hnl.web.app")
+@CrossOrigin(origins = "https://frontend-hnl.web.app/")
 public class CEducacion {
 
     @Autowired
--- src/main/java/com/portfolio/HNL/Controller/PersonaController.java
+++ src/main/java/com/portfolio/HNL/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontend-hnl.web.app")
+@CrossOrigin(origins = "https://frontend-hnl.web.app/")
 public class PersonaController {
 
     @Autowired
