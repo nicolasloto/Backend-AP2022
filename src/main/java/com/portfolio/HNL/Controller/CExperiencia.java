5c2c3733ad43b65d213e0b57abb907b7a511068b
Nicolás Loto <busslxtx@gmail.com>
2022-12-01 19:44:03 -0300

    corrección de origen

--- src/main/java/com/portfolio/HNL/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/HNL/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-hnl.web.app")
 public class CExperiencia {
 
     @Autowired
--- src/main/java/com/portfolio/HNL/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/HNL/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontend-hnl.web.app")
 public class AuthController {
 
     @Autowired
